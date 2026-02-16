ff3b2e87344b83fcc9ffe0aa4675fcdb3750d268
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2026-02-16 18:40:13 +0200

    add the functionality to the toggle

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,1 +1,17 @@
 AOS.init();
+document.querySelector('.collapse-toggle').addEventListener('click', function() {
+  const targetId = this.getAttribute('data-collapse');
+  const targetElement = document.querySelector(targetId);
+
+  if (targetElement) {
+    // تبديل ظهور القائمة
+    targetElement.classList.toggle('hidden');
+    
+    // لإضافة تأثير أنميشن بسيط (اختياري)
+    if (!targetElement.classList.contains('hidden')) {
+      targetElement.style.height = 'auto';
+    } else {
+      targetElement.style.height = '0';
+    }
+  }
+});
